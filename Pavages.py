6a8c6cbb0386544ac067f163c86a201f15c7f343
jbvstein <151051384+jbvstein@users.noreply.github.com>
2024-03-25 16:36:42 +0100

    Create Pavages.py

---
+++ Pavages.py
@@ -0,0 +1,18 @@
+"""
+2. Modélisation et explorations 
+
+(1) : 1 seule dimension, On se place d’abord dans un espace à paver d’une seule dimension
+(un rectangle de longueur n et de hauteur 1). Proposez un algorithme permettant de
+tester si la ligne peut être pavée par un ensemble de polyominos donnés. Si oui, il
+faudra donner la solution.
+
+Vérifier que l'ensemble des polyominos considéré ne possède que des éléments de la forme suivante :
+ dimensions (1,k) avec 1 <= k <= n.
+Si oui,
+
+
+
+
+
+
+"""
