ef3bdf6165fecf5ec9dde14d660c80d374f7ad08
Clara214 <108482424+Clara214@users.noreply.github.com>
2024-03-26 12:37:13 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,95 @@
-#Regle : On représente un polyomino de taille n dans une matrice n*n.
+
+
+import numpy as np
+"""
+class Matrice : 
+    def __init__(self,h=0,l=0):
+        self.hauteur = h
+        self.largeur = l
+        self.plateau = [[0 for i in range(l)] for i in range(h)]
+        return None
+        
+    def addPolyomino(self,poly,x,y) -> None:
+        assert(poly.hauteur-y>0 and h+y
+"""   
+
+
+def fromTabToCoord(tableau):
+    res = []
+    for i in range(len(tableau)):
+        for j in range(len(tableau[i])):
+            if tableau[i][j]==1:
+                res.append((i,j))
+    return res
+    
+def fromCoordToTab(hauteur,largeur,coordonnees):
+     return [[1 if (i,j) in coordonnees else 0 for j in range(largeur)] for i in range(hauteur)]
+ 
+def copie(tab):
+    res =[0 for i in range(len(tab))]
+    for i in range(len(tab)):
+        res[i] = tab[i]
+    return res
+
+     
+class Polyomino:
+    def __init__(self,c=[]):
+        maxH = 0
+        maxL = 0
+        for i in range(len(c)):
+            if(c[i][0]>maxH):
+                maxH = c[i][0]
+            if(c[i][1]>maxL):
+                maxL = c[i][1]
+        self.hauteur = maxH
+        self.largeur = maxL
+        self.taille = len(c)
+        self.coord = c
+            
+            
+        
+    def copie(self):
+        res = Polyomino(self.hauteur, self.largeur, self.taille)
+        res.tableau = [[self.tableau[i][j] for j in range(self.l)] for i in range(self.h)]
+        return res
+        
+    def __str__(self):
+        res = ""
+        tab = fromCoordToTab(self.hauteur+1, self.largeur+1, self.coord)
+        for elem in tab:
+            res+="\n"
+            for val in elem:
+                res += str(val) + " "
+        return res
+        
+    def rotate(self):
+        """Modifie le polyomino en le pivotant de 90 degrés."""
+        tab = fromCoordToTab(self.hauteur+1,self.largeur+1,self.coord)
+        tab_rotate =  np.rot90(np.array(tab))
+        self.coord = fromTabToCoord(tab_rotate)
+        self.hauteur, self.largeur = self.largeur, self.hauteur
+        return None
+    
+    
+    def symetrie(self,axis=0):
+        """Renvoie la symétrie de la matrice. axis = 0 : axe horizontal, axis=1 : axe vertical"""
+        tab = fromCoordToTab(self.hauteur+1,self.largeur+1,self.coord)
+        if axis == 0:
+            for i in range(len(tab)):
+                for j in range(len(tab[i])//2):
+                    tab[i][j], tab[i][len(tab[i])-j-1] = tab[i][len(tab[i])-j-1],tab[i][j]
+        if axis == 1:
+            for i in range(len(tab)//2):
+                tab[i],tab[len(tab)-i-1] = tab[len(tab)-i-1] , tab[i]
+        self.coord = fromTabToCoord(tab)
+        return None
+        
+    
+        
 
 MatL = [[1,0,0,0,0],
         [1,0,0,0,0],
-        [1,1,0,0,0],
-        [0,0,0,0,0],
-        [0,0,0,0,0]]
+        [1,1,0,0,0]]
         
         
 MatC = [[1,1,0,0,0],
@@ -17,152 +102,23 @@ MatI = [[1,0,0,0],
         [1,0,0,0],
         [1,0,0,0],
         [1,0,0,0]]
-        
-def copie(tab):
-    res =[0 for i in range(len(tab))]
-    for i in range(len(tab)):
-        res[i] = tab[i]
-    return res
 
-def rotaMatrice(Mat):
-    """Prend une matrice et la pivote de 90° vers la droite"""
-    res = [ [0 for i in range(len(Mat))] for j in range(len(Mat))]
-    for i in range(len(Mat)):
-        for j in range(len(Mat)):
-            res[j][len(Mat) - i -1] = Mat[i][j]
-    return recentre(res)
-
-def symetrieHorizontale(Mat):
-    res = copie(Mat)
-    for i in range(len(res)//2):
-        res[i],res[len(res)-i-1] = res[len(res)-i-1] , res[i]
-    return recentre(res)
-        
-def symetrieVerticale(Mat):
-    res = copie(Mat)
-    for i in range(len(res)):
-        for j in range(len(res[i])//2):
-            res[i][j], res[i][len(res[i])-j-1] = res[i][len(res[i])-j-1],res[i][j]
-    return recentre(res)
-    
-    
-def decaleHaut(Mat, compt):
-    res = copie(Mat)
-    for i in range(compt, len(Mat)):
-        res[i-compt] = res[i]
-    for k in range(len(Mat) - compt, len(Mat)):
-        res[k] = [0 for j in range (len(Mat[0]))]
-    return res
-    
-
-
-def decaleGauche(Mat, compt):
-    res = copie(Mat)
-    for i in range(len(Mat)):
-        for j in range(compt,len(Mat[i])):
-            res[i][j-compt] = res[i][j]
-        for j in range(len(Mat[i]) - compt, len(Mat[i])):
-            res[i][j] = 0
-    return res
-    
-
-
-def recentre(Mat):
-    tmp = copie(Mat)
-    centreHaut = False
-    centreGauche = False
-    comptGauche = 0
-    comptHaut = 0
-    ligne = 0
-    colonne = 0
-    while not centreHaut:
-        if Mat[ligne][colonne]!=0:
-            centreHaut = True
-            
-        elif colonne<len(Mat)-1 :
-            colonne+=1
-        else:
-            colonne=0
-            ligne+=1
-            comptHaut+=1
-    MatHaut = decaleHaut(tmp, comptHaut)
-    ligne = 0
-    colonne = 0
-    while not centreGauche:
-        if Mat[ligne][colonne] != 0:
-            centreGauche = True
-            
-        elif ligne <len(Mat) - 1:
-            ligne +=1
-        else:
-            ligne = 0
-            colonne += 1
-            comptGauche += 1
-        
-    MatGauche = decaleGauche(MatHaut, comptGauche)
-        
-    return MatGauche
-
-        
+test = [[0,0, 1],[1,1,1]]
 """
-#Rotation
-
-a = rotaMatrice(MatL)
-b = rotaMatrice(a)
-c = rotaMatrice(b)
-d = rotaMatrice(c)
-
-
-a_bis  = symetrieHorizontale(a)
-
-for elem in MatL:
-    print(elem)
-print("-----a--------")
-for elem in a :
-    print(elem)
-print("------a bis-------")
-for elem in a_bis:
-    print(elem)
-
-
-print("-------b -------")
-for elem in b :
-    print(elem)
-print("--------------")
-for elem in c :
-    print(elem)
-print("--------------")
-for elem in d :
-    print(elem)
-print("--------------")
-"""
-
-#Symetrie
-
-a = symetrieHorizontale(MatL)
-
-b = symetrieVerticale(MatL)
-
-
-for elem in MatL:
-    print(elem)
-
-
-print("-----SYMETRIE HORIZONTALE------")
-for elem in a:
-    print(elem)
-
-print("----SYMETRIE VERTICALE-------")
-for elem in b:
-    print(elem)
-
-"""
-test = [1,3,2,4,9]
-for i in range(len(test)//2):
-    test[i], test[len(test)-i-1] = test[len(test)-i-1],test[i]
-print(test)
-"""               
-                
+for elem in test:
+    print(elem)"""
+coordL = fromTabToCoord(MatL)
+tab = fromCoordToTab(3, 2, coordL)
 
 
 
+a = Polyomino(coordL)
+print(a)
+a.symetrie(0)
+
+print(a)
+
+    
+
+        
+        
