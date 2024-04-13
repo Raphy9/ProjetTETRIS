2672fa10259bdd6b2ea22fa5c000951a0d2af8ea
jbvstein <151051384+jbvstein@users.noreply.github.com>
2024-04-13 18:33:17 +0200

    Update Pavages.py

--- Pavages.py
+++ Pavages.py
@@ -9,10 +9,20 @@ faudra donner la solution.
 Vérifier que l'ensemble des polyominos considéré ne possède que des éléments de la forme suivante :
  dimensions (1,k) avec 1 <= k <= n.
 Si oui,
-
-
-
-
-
-
 """
+
+def Pavage_1D(n=4, t=[1, 2, 3]):
+ """
+paramètres : 
+- n un entier représentant la longueur du rectangle à paver 
+- t un tableau d'entiers representant l'ensemble des longueurs des polyominos rectangulaires de la forme (1,k)
+return : le nombre de manières de paver le rectangle considéré
+ """
+#coinSizes = [1, 2, 5, 10, 20, 50]
+ways = [0] * (n + 1)
+ways[0] = 1
+for l in t:
+    for i in range(l, n+1):
+        ways[i] += ways[i-l]
+    print(ways)
+print(f"Il y a {ways[-1]} façons de paver {n} avec des polyominos de longueur {t}")
