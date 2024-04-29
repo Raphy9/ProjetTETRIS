2a97ada82db609a987aebc36fae78d33d063fd18
uGriss <147511418+uGriss@users.noreply.github.com>
2024-04-29 16:30:37 +0200

    Update Pavages2D.py

--- Pavages2D.py
+++ Pavages2D.py
@@ -148,18 +148,19 @@ for l in t:
 print(f"Il y a {ways[-1]} façons de paver {n} avec des polyominos de longueur {t}")
 """
 
+
 def est_valide(grille, polyomino, x, y):
     for elem in polyomino.coord:
-        if x + elem[0] >= len(grille) or y + elem[1] >= len(grille[x]):
+        if x + elem[0] >= len(grille) or y + elem[1] >= len(grille[x + elem[0]]):
             return False
         if grille[x + elem[0]][y + elem[1]] != 0:
             return False
     return True
 
 def placer_polyomino(grille, polyomino, x, y, id_polyomino):
-    id_bis = id_polyomino + len(grille)^2 * (id_polyomino % 2) + x + y
+    #id_bis = id_polyomino + len(grille)^2 * (id_polyomino % 2) + x + y
     for elem in polyomino.coord:
-        grille[x + elem[0]][y + elem[1]] = id_bis
+        grille[x + elem[0]][y + elem[1]] = id_polyomino
     return grille
 
 def retirer_polyomino(grille, polyomino, x, y):
@@ -187,11 +188,14 @@ def trouver_pavage(grille, polyominos, id_polyomino=1):
     return True
 
 taille_grille = 10
-grille = [[0 for I1Il1l1I in range(taille_grille)] for _ in range(taille_grille - 1)]
+grille = [[0 for I1Il1l1I in range(taille_grille - x)] for x in range(taille_grille - 5)]
+for hihi in grille:
+    print(hihi)
 #polyominos = [Polyomino([(0, 0), (0,1),(1,0),(1,1)]), Polyomino([(0,0),(1,0),(0,1)])]
 Polyomino_L = Polyomino([(0,0), (1,0), (0,1), (0,2)])
 polyominos = Polyomino_L.classeComplete()
 #polyominos.append(Polyomino([(0,0), (0,1), (1,0), (1,1)]))
+polyominos.append(Polyomino([(0,0)]))
 
 
 
@@ -199,11 +203,11 @@ if trouver_pavage(grille, polyominos):
     print("Tiling possible")
 else:
     print("No tiling found")
-"""for hihi in grille:
-    print(hihi)"""
+for hihi in grille:
+    print(hihi)
 
-plt.matshow(grille)
-plt.show()
+#plt.matshow(grille)
+#plt.show()
 """print(type(polyominos))
 print(polyominos[0])
 """
